2b27e9bd3ae2ddf0c1c84a0584b582443fed7570
PufSunCat <114050978+PufSunCat@users.noreply.github.com>
2022-09-28 08:18:15 +0300

    Update GamePie.py

--- GamePie.py
+++ GamePie.py
@@ -1,13 +1,24 @@
-human_turn = 'rock'
-computer_turn = 'scissors'
 
-if human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'rock' and computer_turn == 'scissors':
-    print('Human wins!')
-elif human_turn == 'paper' and computer_turn == 'rock':
-    print('Human wins!')
-elif human_turn == 'scissors' and computer_turn == 'paper':
-    print('Human wins!')
-else:
-    print('Computer wins!')
+import random
+turns = ['rock', 'paper', 'scissors']
+
+while(True):
+    
+    human_turn = input("Enter your turn, or tipe exit:")
+    computer_turn = random.choice(turns)
+    
+    if human_turn == 'exit':
+        print("thx for playing")
+        break
+
+    print(f'{human_turn}vs.{computer_turn}')
+    if human_turn == computer_turn:
+        print('Draw!')
+    elif human_turn == 'rock' and computer_turn == 'scissors':
+        print('Human wins!')
+    elif human_turn == 'paper' and computer_turn == 'rock':
+        print('Human wins!')
+    elif human_turn == 'scissors' and computer_turn == 'paper':
+        print('Human wins!')
+    else:
+        print('Computer wins!')
